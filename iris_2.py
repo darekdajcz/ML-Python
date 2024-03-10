8db8a5a9dc791cd0e61808806198e9651164ed29
darekdajcz <daarek.dajcz@gmail.com>
2024-03-10 15:48:33 +0100

    iris-2

--- iris_2.py
+++ iris_2.py
@@ -1,17 +1,16 @@
 
 
-                   # Ładowanie bibliotek
+  
 import pandas as pd
 import matplotlib.pyplot as plt
 %matplotlib inline
 from sklearn.linear_model import LinearRegression
  
-# Ładowanie danych
+
 auto = pd.read_csv(r"/Users/darekdajcz/Desktop/Uczenie_maszynowe_Python/Kurs ML/auto-mpg.csv")
 auto.head()
 auto.shape
  
-# Przygotowanie danych
 X = auto.iloc[:, 1:-1]
 X = X.drop('horsepower', axis=1)
 y = auto.loc[:,'mpg']
@@ -19,12 +18,10 @@ y = auto.loc[:,'mpg']
 X.head()
 y.head()
  
-# Budowanie modelu
 lr =  LinearRegression()
 lr.fit(X.to_numpy(),y)
 lr.score(X.to_numpy(),y)
  
-# Korzystanie z modelu
 my_car1 = [4, 160, 190, 12, 90, 1]
 my_car2 = [4, 200, 260, 15, 83, 1]
 cars = [my_car1, my_car2]
