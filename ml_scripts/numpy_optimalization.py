37d6ee50645634ac6dbc53181914cd4c5cb4b58f
darekdajcz <daarek.dajcz@gmail.com>
2024-03-13 16:43:37 +0100

    numpy optimalization

---
+++ ml_scripts/numpy_optimalization.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 16:29:45 2024
+
+@author: darekdajcz
+"""
+
+import time 
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+num_iterations = 100
+time_results_loop = []
+
+for iteration in range(1, num_iterations+1):
+    start_time = time.time()
+    data = np.arange(0, 10000 * iteration, 1)
+    
+    sum = 0 
+    for i in data: 
+        sum += i 
+    end_time = time.time()
+    
+    print('{} - {}'.format(iteration, end_time - start_time))
+    time_results_loop.append(end_time - start_time)
+    
+    
+## optimalization
+for iteration in range(1, num_iterations+1):
+    start_time = time.time()
+    
+    data = np.arange(0, 10000 * iteration, 1)
+    
+    ## difference
+    sum = np.sum(data) 
+  
+    end_time = time.time()
+    
+    print('{} - {}'.format(iteration, end_time - start_time))
+    time_results_loop.append(end_time - start_time)
